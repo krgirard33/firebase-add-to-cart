8568fc817d6cbb99016525a73971bf40deaae822
Ken <ken.girard@gmail.com>
2023-07-22 20:05:31 -0500

    Feeds info to db, and post entry to html

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://playground-6336c-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDb = ref(database, "shoppingList");
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
-  console.log(inputValue);
+  push(shoppingListInDb, inputValue);
+  inputFieldEl.value = "";
+  shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
 });
